e3e1dc78f32f0880599bea7906314494a6db7229
bryanthr6 <2988707890101@ingenieria.usac.edu.gt>
2024-09-24 19:58:41 -0600

    De momento se quitó todo a excepción del app y template

--- nodo_componente.py
+++
@@ -1,5 +0,0 @@
-class NodoComponente:
-    def __init__(self, numero, tiempo_ensamblaje):
-        self.numero = numero
-        self.tiempo_ensamblaje = tiempo_ensamblaje
-        self.siguiente = None  # Puntero al siguiente componente
